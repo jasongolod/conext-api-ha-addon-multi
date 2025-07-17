969e4cae5cfd31eea1febc8a1777384e9bfc8cac
jasongolod <jason@golod.com>
2025-07-17 15:33:02 -0500

    Update run.sh

--- conext_api_multi/run.sh
+++ conext_api_multi/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/with-contenv bashio
 
 echo "Starting run.sh - Generating config.json"
-bashio::config 'gateways' | jq > /app/config.json || echo "Error generating config.json from bashio"
-
+bashio::config > /app/raw_config.json || echo "Error fetching bashio config"
+cat /app/raw_config.json || echo "Error reading raw_config.json"
+jq '.gateways // []' /app/raw_config.json > /app/config.json || echo "Error processing config.json with jq"
 echo "Starting NGINX"
 nginx || echo "Error starting NGINX"
-
 echo "Starting Flask app"
 python3 /app/solarmonitor.py || echo "Error starting solarmonitor.py"
