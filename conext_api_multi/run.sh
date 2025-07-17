3d217eb774403160f6f83b9ad28a9574e23c7512
jasongolod <jason@golod.com>
2025-07-17 13:40:36 -0500

    Update run.sh

--- conext_api_multi/run.sh
+++ conext_api_multi/run.sh
@@ -3,8 +3,11 @@
 # Generate config.json from add-on options
 bashio::config 'gateways' | jq > /app/config.json
 
-# Start NGINX
-nginx
+# Start NGINX in background
+nginx &
 
-# Start the Flask app with gunicorn
-gunicorn -b 0.0.0.0:5000 -w 4 solarmonitor:app
+# Short delay to ensure NGINX starts
+sleep 1
+
+# Start the Flask app with gunicorn (4 workers for better handling)
+gunicorn --bind 0.0.0.0:5000 --workers 4 solarmonitor:app
